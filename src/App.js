20feae86ff0f17075870660120af8bba6eef8ba6
HC mbp21 <harrchiu@gmail.com>
2022-02-27 19:55:23 -0500

    file creation, re-yarned i think

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './HomePage'
+
 function App() {
   return (
     <div className="App">
---
+++ src/HomePage/index.tsx
@@ -0,0 +1,6 @@
+const HomePage = () => {
+
+    return <div>halsdkjf</div>
+}
+
+export default HomePage
